59a943cd75f6eaa60e9df3f81bfbcb372de6aac7
demonic-guru <40889641+demonic-guru@users.noreply.github.com>
2018-07-08 18:02:16 +0530

    Update testz.sh

--- testz.sh
+++ testz.sh
@@ -2,6 +2,15 @@ cd
 wget https://github.com/zealiumcoin/Zealium/releases/download/v1.0.0.0/Zealium-v1.0.0.0-linux64.tar.gz
 tar xvf Zealium-v1.0.0.0-linux64.tar.gz
 
+# The RPC node will only accept connections from your localhost
+_rpcUserName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '')
+
+# Choose a random and secure password for the RPC
+_rpcPassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
+
+# Get the IP address of your vps which will be hosting the smartnode
+_nodeIpAddress=$(ip route get 1 | awk '{print $NF;exit}')
+
 # Make a new directory for zealium daemon
 mkdir ~/.zealium/
 touch ~/.zealium/zealium.conf
@@ -22,9 +31,12 @@ sleep 10s
 masternodekey=$(./zealium-cli masternode genkey)
 ./zealium-cli stop
 sleep 10s
-echo -e "masternode=1\masternodeprivkey=$masternodekey" >> /root/.zealium/zealium.conf
+echo -e "masternode=1" >> /root/.zealium/zealium.conf
+echo -e "masternodeprivkey=$masternodekey" >> /root/.zealium/zealium.conf
 
 cd
 cd bin/
 ./zealiumd
 echo "VPS setup is completed"
+sleep 10s
+./zealium-cli getinfo
