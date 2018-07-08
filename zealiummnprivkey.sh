8f277cd3d070fb5e731f342dff65ff82ddd76ac6
demonic-guru <40889641+demonic-guru@users.noreply.github.com>
2018-07-08 11:41:33 +0530

    Create zealiummnprivkey.sh

---
+++ zealiummnprivkey.sh
@@ -0,0 +1,24 @@
+# Get a new privatekey by going to console >> debug and typing smartnode genkey
+#printf "Masternode GenKey: "
+#read _nodePrivateKey
+
+#Generate New Masternode Privkey and reconfigure zealium.conf
+_MNPRIVKEY=$(zealium-cli masternode genkey)
+read _MNPRIVKEY
+zealium-cli stop
+rm ~/.zealium/zealium.conf
+
+# Change the directory to ~/.zealium
+cd ~/.zealium/
+
+# Create the FINAL zealium.conf file
+echo "rpcuser=${_rpcUserName}
+rpcpassword=${_rpcPassword}
+rpcallowip=127.0.0.1
+rpcport=31090
+listen=1
+server=1
+daemon=1
+masternode=1
+masternodeprivkey=${_MNPRIVKEY}
+" > zealium.conf
